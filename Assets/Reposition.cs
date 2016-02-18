34d6ef67371f2e45d1f6d31ac88e239170d78471
WILLIAM POWELL (RIT Student) <wap2895@ad.rit.edu>
2016-02-18 16:03:20 -0500

    fixed gitignore, removed extra libraries, seeking example

---
+++ Assets/Reposition.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reposition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(col.gameObject);
+        if(col.gameObject.name == "Sphere")
+        this.transform.position = new Vector3(Random.Range(-15, 15),0, Random.Range(-15, 15));
+    }
+}
